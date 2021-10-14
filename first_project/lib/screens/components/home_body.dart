9c1cf7d1aa323dffd8cc76bb14de2557a4801cf2
LilChik <galdaff15@gmail.com>
2021-10-14 16:34:57 +0700

    update home_body

--- first_project/lib/screens/components/home_body.dart
+++ first_project/lib/screens/components/home_body.dart
@@ -1,16 +1,11 @@
-import 'package:first_project/constants.dart';
-import 'package:first_project/screens/components/section_titles.dart';
-import 'package:first_project/screens/models/Product.dart';
 import 'package:first_project/size_config.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 import 'categories.dart';
 import 'discount_banner.dart';
 import 'home_header.dart';
 import 'popular_product.dart';
-import 'product_card.dart';
 import 'special_offers.dart';
 
 class HomeBody extends StatelessWidget {
